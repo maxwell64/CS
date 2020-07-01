7db2bb2cad2e57678991cbfcaa528eb2218a0d34
Max Martin <53559297+maxwell64@users.noreply.github.com>
2020-07-01 13:49:13 +0100

    Add files via upload

---
+++ election_tracker/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace election_tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Candidate> candidate_list = new List<Candidate>();
+            candidate_list.Add(new Candidate("Jones"));
+            Console.Write("Which candidate would you like to vote for?: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("Congratulations! You voted for " + input);
+            if (check_name(input, candidate_list) == false)
+            {
+                candidate_list.Add(new Candidate(input));
+            }
+            foreach (var i in candidate_list)
+            {
+                if (input.Equals(i.name))
+                {
+                    i.votes ++;
+                }
+                Console.WriteLine(i.name + " : " + i.votes);
+            }
+        }
+        public class Candidate
+        {
+            public int votes = 0;
+            public string name;
+            public Candidate(string n)
+            {
+                name = n;
+            }
+        }
+        static bool check_name(string name, List<Candidate> list)
+        {
+            foreach (Candidate i in list)
+            {
+                if (i.name.Equals(name))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
